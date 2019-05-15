1754a223e76e1cabed9c7959f3a48a3f2251716b
jarrensj <sanjose.jarren@gmail.com>
2019-05-15 11:03:58 -0700

    resize on window resize

--- main.js
+++ main.js
@@ -28,5 +28,13 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
 init();
 animate();
